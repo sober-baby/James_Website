1920164a2a9811b2381202318fd04776fdcf15a2
sober-baby <61091443+sober-baby@users.noreply.github.com>
2025-03-20 16:34:02 -0400

    modified linkes

--- website/src/components/ResumeProjectsSection.js
+++ website/src/components/ResumeProjectsSection.js
@@ -5,15 +5,15 @@ import Button from "./Buttons";
 
 function ResumeProjectsSection() {
   const handleResumeClick = () => {
-    window.open("https://example.com/resume.pdf", "_blank");
+    window.open("https://drive.google.com/file/d/1wLOPgjgR4Fkh0uIvp2wCUtClAM_ew6AM/view?usp=sharing", "_blank");
   };
 
   const handleFTCClick = () => {
-    window.open("https://example.com/ftc_robotics_video", "_blank");
+    window.open("https://https://docs.google.com/presentation/d/1lM3jj8pSYZxKxMdmmpFY-rde9vLEl-IzZ9mQz241J2M/edit?usp=sharing.com/ftc_robotics_video", "_blank");
   };
 
   const handleDayTravelMapClick = () => {
-    window.open("https://example.com/day_travel_map", "_blank");
+    window.open("https://docs.google.com/presentation/d/1vgAb0SBaMucDCLXPwO3QGNEcW5XXL0kilnCG-Ei4qSY/edit?usp=sharing", "_blank");
   };
 
   return (
